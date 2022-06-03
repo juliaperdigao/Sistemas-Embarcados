3b423753efc2ad8bde4151059dc4e256b796de19
Julia Perdigão <67842041+juliaperdigao@users.noreply.github.com>
2022-06-02 22:57:09 -0300

    Update trabSE.ino

--- trabSE.ino
+++ trabSE.ino
@@ -24,6 +24,7 @@ void setup() {
   myServo.attach(12);// Defines on which pin is the servo motor attached
   pinMode(8,OUTPUT);
 }
+
 void loop() {
   // rotates the servo motor from 15 to 165 degrees
   for(int i=15;i<=165;i++){  
@@ -41,7 +42,6 @@ void loop() {
   Serial.print(distance); // Sends the distance value into the Serial Port
   Serial.println(" cm"); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
   }
-  
   // Repeats the previous lines from 165 to 15 degrees
   for(int i=165;i>15;i--){  
   myServo.write(i);
@@ -59,9 +59,9 @@ void loop() {
   Serial.println(" cm");
   }
 }
+
 // Function for calculating the distance measured by the Ultrasonic sensor
-int calculateDistance(int trig, int echo){ 
-  
+int calculateDistance(int trig, int echo){   
   digitalWrite(trig, LOW); 
   delayMicroseconds(2);
   // Sets the trigPin on HIGH state for 10 micro seconds
